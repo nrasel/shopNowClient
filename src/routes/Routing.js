b8d0e5a60b5669c38e2c2919665233068a689640
nrasel452 <nrasel452@gmail.com>
2022-03-07 01:14:26 +0600

    first step

---
+++ src/routes/Routing.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminLogin from "../screens/auth/AdminLogin";
+import Products from "../screens/dashboard/Products";
+
+const Routing = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="auth">
+          <Route path="admin-login" element={<AdminLogin />} />
+        </Route>
+        <Route path="dashboard">
+          <Route path="products" element={<Products />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routing;
---
+++ src/screens/dashboard/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <h1>products lists</h1>
+    </div>
+  );
+};
+
+export default Products;
